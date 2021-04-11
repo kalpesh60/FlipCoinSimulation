650e6a1cdb2b2a44444fe5e144452d6e826191f8
kalpesh60 <kalpeshdhalpe60@gmail.com>
2021-04-11 23:17:53 +0530

    added: flipped the coin to get result

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,1 +1,12 @@
+#!/bin/bash -x
 echo "Welcome to Flip Coin Simulation Problem"
+heads=0;
+
+flip=$(($RANDOM%2))
+
+if [[ $flip -eq $heads ]]
+then
+	echo "Winner is Heads"
+else
+	echo "Winner is Tails"
+fi
